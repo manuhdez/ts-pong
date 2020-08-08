2d91f080d6993ee1bacb2118c5b19f5a3112d744
Manu Hdez <manu.hdez.dev@gmail.com>
2020-08-08 01:25:58 +0100

    Initial parcel setup

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootComponent = <h1>Hello pong!</h1>;
+const rootContainer = document.getElementById('root');
+
+ReactDOM.render(rootComponent, rootContainer);
